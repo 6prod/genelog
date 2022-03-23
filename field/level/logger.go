170622b42da7365f1a7da5165f748cdabff875fa
Stefan Bourlon <s.bourlon@gmail.com>
2022-03-22 21:54:18 -0700

    field/level: remove LevelLogger interface
    not useful at this level

--- field/level/logger.go
+++ field/level/logger.go
@@ -2,94 +2,72 @@ package level
 
 import "github.com/6prod/genelog"
 
-type LevelLogger interface {
-	Info(v ...interface{})
-	Infoln(v ...interface{})
-	Infof(format string, v ...interface{})
-
-	Error(v ...interface{})
-	Errorln(v ...interface{})
-	Errorf(format string, v ...interface{})
-
-	Debug(v ...interface{})
-	Debugln(v ...interface{})
-	Debugf(format string, v ...interface{})
-
-	Warning(v ...interface{})
-	Warningln(v ...interface{})
-	Warningf(format string, v ...interface{})
-
-	Fatal(v ...interface{})
-	Fatalln(v ...interface{})
-	Fatalf(format string, v ...interface{})
-}
-
-type loggerWithLevel struct {
+type LevelLogger struct {
 	*genelog.Logger
 }
 
-func NewLevelLogger(logger *genelog.Logger) loggerWithLevel {
-	return loggerWithLevel{
+func NewLevelLogger(logger *genelog.Logger) LevelLogger {
+	return LevelLogger{
 		logger,
 	}
 }
 
-func (l loggerWithLevel) Info(v ...interface{}) {
+func (l LevelLogger) Info(v ...interface{}) {
 	Info(l.Logger, v...)
 }
 
-func (l loggerWithLevel) Infoln(v ...interface{}) {
+func (l LevelLogger) Infoln(v ...interface{}) {
 	Infoln(l.Logger, v...)
 }
 
-func (l loggerWithLevel) Infof(format string, v ...interface{}) {
+func (l LevelLogger) Infof(format string, v ...interface{}) {
 	Infof(l.Logger, format, v...)
 }
 
-func (l loggerWithLevel) Error(v ...interface{}) {
+func (l LevelLogger) Error(v ...interface{}) {
 	Error(l.Logger, v...)
 }
 
-func (l loggerWithLevel) Errorln(v ...interface{}) {
+func (l LevelLogger) Errorln(v ...interface{}) {
 	Errorln(l.Logger, v...)
 }
 
-func (l loggerWithLevel) Errorf(format string, v ...interface{}) {
+func (l LevelLogger) Errorf(format string, v ...interface{}) {
 	Errorf(l.Logger, format, v...)
 }
 
-func (l loggerWithLevel) Debug(v ...interface{}) {
+func (l LevelLogger) Debug(v ...interface{}) {
 	Debug(l.Logger, v...)
 }
 
-func (l loggerWithLevel) Debugln(v ...interface{}) {
+func (l LevelLogger) Debugln(v ...interface{}) {
 	Debugln(l.Logger, v...)
 }
 
-func (l loggerWithLevel) Debugf(format string, v ...interface{}) {
+func (l LevelLogger) Debugf(format string, v ...interface{}) {
 	Debugf(l.Logger, format, v...)
 }
 
-func (l loggerWithLevel) Warning(v ...interface{}) {
+func (l LevelLogger) Warning(v ...interface{}) {
 	Warning(l.Logger, v...)
 }
 
-func (l loggerWithLevel) Warningln(v ...interface{}) {
+func (l LevelLogger) Warningln(v ...interface{}) {
 	Warningln(l.Logger, v...)
 }
 
-func (l loggerWithLevel) Warningf(format string, v ...interface{}) {
+func (l LevelLogger) Warningf(format string, v ...interface{}) {
 	Warningf(l.Logger, format, v...)
 }
 
-func (l loggerWithLevel) Fatal(v ...interface{}) {
+func (l LevelLogger) Fatal(v ...interface{}) {
 	Fatal(l.Logger, v...)
 }
 
-func (l loggerWithLevel) Fatalln(v ...interface{}) {
+func (l LevelLogger) Fatalln(v ...interface{}) {
 	Fatalln(l.Logger, v...)
 }
 
-func (l loggerWithLevel) Fatalf(format string, v ...interface{}) {
+func (l LevelLogger) Fatalf(format string, v ...interface{}) {
 	Fatalf(l.Logger, format, v...)
 }
