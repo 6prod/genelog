a4dc61ddab6d74261c4288dd7d727caf343fe6b9
Stefan Bourlon <s.bourlon@gmail.com>
2022-04-10 16:46:55 -0700

    format/json: add note about embedded structs issue

--- format/json/json.go
+++ format/json/json.go
@@ -1,3 +1,25 @@
+// Package JSON formats log outputs into JSON using
+// the structure { "context": {}, "message": "message" }
+//
+// Note that the encoding/json package used underneath
+// does not work with embbeded structures until the go
+// issue https://github.com/golang/go/issues/6213
+// is solved.
+//
+// A temporary solution is to define the MarshalJSON() ([]byte, error)
+// method on the context type.
+//
+// Example:
+//
+// func (c Context) MarshalJSON() ([]byte, error) {
+// 	return libjson.Marshal(struct {
+// 		Time  libtime.Time `json:"time"`
+// 		Level level.Level  `json:"level"`
+// 	}{
+// 		Time:  c.WithTime.Time(),
+// 		Level: c.WithLevel.Level(),
+// 	})
+// }
 package json
 
 import (
